8433ae3d9a5ec51d54c40237a7d78350e89bbcc7
Ahanuf Hossain <athossai@asu.edu>
2016-02-10 20:29:44 -0700

    Prints out the first task that is found

--- wip_kernelmods/wip_mod.c
+++ wip_kernelmods/wip_mod.c
@@ -2,9 +2,26 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+#include <linux/sched.h>
+#include <linux/string.h>
+
 int __init wip_init(void)
 {
+	struct thread_info *threadinfo;
+	struct task_struct *tasks;
+	char comm[TASK_COMM_LEN];
+
 	printk(KERN_ALERT "Inserted wip_mod\n");
+
+	// get current tasks
+	threadinfo = current_thread_info();
+	tasks = threadinfo->task;
+
+	// probably init?
+	strcpy(comm, tasks->comm);
+
+	printk(KERN_ALERT "task name --> %s\n", comm);
+
 	return 0;
 }
 
