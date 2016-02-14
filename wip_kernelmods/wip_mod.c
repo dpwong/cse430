9d6e8699460d96378ca98db39ba349db53ce3929
Ahanuf Hossain <athossai@asu.edu>
2016-02-13 17:22:28 -0700

    All tasks now get stored in task_list (which is an array of task_info, a custom struct). This should make it easier to copy to the user

--- wip_kernelmods/wip_mod.c
+++ wip_kernelmods/wip_mod.c
@@ -4,23 +4,50 @@
 
 #include <linux/sched.h>
 #include <linux/string.h>
+#include <linux/slab.h>
+
+struct task_info {
+	pid_t pid;
+	// cputime_t stime;
+	char comm[TASK_COMM_LEN];
+	// struct signal_struct *signal;
+};
 
 int __init wip_init(void)
 {
-	struct thread_info *threadinfo;
-	struct task_struct *tasks;
-	char comm[TASK_COMM_LEN];
-
+	struct task_info **task_list;
+	struct task_info *new_task;
+	struct task_info temp_task;
+	struct task_struct *task;
+	
+	unsigned int count = 0;
+	int i = 0;
 	printk(KERN_ALERT "Inserted wip_mod\n");
 
-	// get current tasks
-	threadinfo = current_thread_info();
-	tasks = threadinfo->task;
+	task = current;
+	for_each_process(task)
+	{
+		count++;
+	}
 
-	// probably init?
-	strcpy(comm, tasks->comm);
+	task_list = (struct task_info **)kmalloc(count * sizeof(struct task_info), GFP_KERNEL);
 
-	printk(KERN_ALERT "task name --> %s\n", comm);
+
+	for_each_process(task)
+	{
+		new_task = (struct task_info *)kmalloc(sizeof(struct task_info), GFP_KERNEL);
+		strcpy(new_task->comm, task->comm);
+		task_list[i] = new_task;
+		kfree(new_task);
+		i++;
+	}
+	task = NULL;
+
+	// Now that everything is in the array,
+	// it's time to print the last one to check
+	printk(KERN_ALERT "There were %u processes\n", count);
+	temp_task = *(task_list[count - 1]);
+	printk(KERN_ALERT "The last process was %s\n", temp_task.comm);
 
 	return 0;
 }
