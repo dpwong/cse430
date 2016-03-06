4e2d02ab3be393e19c5dcc88e5277b9fefc265a7
dpwong <daniel.p.wong2@gmail.com>
2016-03-05 21:20:04 -0700

    Added files via upload

---
+++ Part2TestCases/fork_binary.c
@@ -0,0 +1,50 @@
+/*
+ * fork_binary.c
+ *
+ *  Created on: Dec 25, 2015
+ *      Author: duolu
+ */
+
+#include <stdio.h>
+
+#include <unistd.h>
+
+
+
+
+int main(int argc, char **argv)
+{
+	int i;
+	pid_t pid;
+
+	if(argc < 2) {
+		printf("Usage: fork_binary n\n");
+		return 0;
+	}
+
+	int n = strtol(argv[1], NULL, 10);
+
+	int p = getpid();
+	printf("%d\n", p);
+	fflush(stdout);
+
+	char new_n[4];
+	snprintf(new_n, sizeof(new_n), "%d", n - 1);	
+
+	if(n > 0) {
+
+    		pid = fork();
+    		if(pid == 0){
+    			execl(argv[0], argv[0], new_n, NULL);
+    		}
+    		pid = fork();
+    		if(pid == 0){
+    			execl(argv[0], argv[0], new_n, NULL);
+    		}
+	}
+ 
+    	while(1)
+    		;
+	return 0;
+}
+
---
+++ Part2TestCases/fork_long_tail.c
@@ -0,0 +1,46 @@
+/*
+ * fork_long_tail.c
+ *
+ *  Created on: Dec 25, 2015
+ *      Author: duolu
+ */
+
+#include <stdio.h>
+
+#include <unistd.h>
+
+
+
+
+int main(int argc, char **argv)
+{
+	int i;
+	pid_t pid;
+
+	if(argc < 2) {
+		printf("Usage: fork_long_tail n\n");
+		return 0;
+	}
+
+	int n = strtol(argv[1], NULL, 10);
+
+	int p = getpid();
+	printf("%d\n", p);
+	fflush(stdout);
+
+	char new_n[4];
+	snprintf(new_n, sizeof(new_n), "%d", n - 1);	
+
+	if(n > 0) {
+
+    		pid = fork();
+    		if(pid == 0){
+    			execl(argv[0], argv[0], new_n, NULL);
+    		}
+	}
+ 
+    	while(1)
+    		;
+	return 0;
+}
+
